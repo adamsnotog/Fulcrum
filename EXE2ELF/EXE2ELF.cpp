18b99a87283e8b2fd4148393202492487dbe2533
adamsnotog <adamsplus1945@gmail.com>
2026-04-14 16:40:18 +0200

    Modify copyright and enhance error handling
    Updated copyright information and added error handling for ELF writing process.

--- EXE2ELF/EXE2ELF.cpp
+++ EXE2ELF/EXE2ELF.cpp
@@ -1,7 +1,7 @@
 //EXE2ELF
 //Prototype
-//© (Repository(https://github.com/adamsnotog/EXE2ELF) creation date) 2025 All credits and rights to Adams @adamsplus1945
-//GPLv3
+//© (Repository(https://github.com/adamsnotog/Fulcrum) creation date) 2025 All credits and rights to Adams @adamsplus1945
+//GPLv3 
 //Code highly sensitive to Original Program alignment and EXE structure standards
 //Doesn't support Arm(Bus Error) Rom Big endian
 #include"Reading.h"
@@ -10,13 +10,39 @@
 int main(){
 cout<<BLUE<<"Enter path of exe file: ";
 char a=name_insert();
+int b;
 if(a==0){
-reading();
-elf_writing();
+b=reading();
 }
-	
-	
-	
-	
-	
+if(b==0){
+char c=elf_writing();	
+if(c==0){
+struct termios tty;
+tcgetattr(STDIN_FILENO, &tty);
+tty.c_lflag&= ~ECHO;
+tty.c_lflag &= ~ICANON;
+cout<<"\033[?25l";
+tcsetattr(STDIN_FILENO, TCSANOW, &tty); 
+cout<<BLUE<<"\n\nProcess Has Done Successfully."<<RESET;
+}
+else if(c==20){
+struct termios tty;
+tcgetattr(STDIN_FILENO, &tty);
+tty.c_lflag&= ~ECHO;
+tty.c_lflag &= ~ICANON;
+cout<<"\033[?25l";
+tcsetattr(STDIN_FILENO, TCSANOW, &tty); 
+cout<<RED<<"\n\nSection size is so big or the section is corrupted";
+}
+else if(c==3){
+struct termios tty;
+tcgetattr(STDIN_FILENO, &tty);
+tty.c_lflag&= ~ECHO;
+tty.c_lflag &= ~ICANON;
+cout<<"\033[?25l";
+tcsetattr(STDIN_FILENO, TCSANOW, &tty); 
+cout<<RED<<"\nCritical Error:No Executable Code Found";
+}
+}
+cout<<RESET;
 }
